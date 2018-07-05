9a71d3b5e59d95a43415fbc6866098a60967dbf6
menibesso <meni.b@qualisystems.com>
2018-07-05 13:48:52 +0300

    update

---
+++ applications/wordpress/wordpress-start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "****************************************************************"
+echo "Restarting Apache"
+echo "****************************************************************"
+systemctl restart apache2.service
